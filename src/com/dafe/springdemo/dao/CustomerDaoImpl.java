074c86781fae42fbcae118ad6edf663f61ad135a
Iredafe Owolabi <iredafeowolabi@gmail.com>
2020-03-06 15:36:28 -0800

    write code for customer dao implementation class

--- src/com/dafe/springdemo/dao/CustomerDaoImpl.java
+++ src/com/dafe/springdemo/dao/CustomerDaoImpl.java
@@ -1,5 +1,43 @@
 package com.dafe.springdemo.dao;
 
-public class CustomerDaoImpl {
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dafe.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDaoImpl implements CustomerDao{
+
+	//need to inject the session factory
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		
+		//get the current hibernate session
+
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		//create a query
+		
+		Query <Customer> theQuery =currentSession.createQuery("from Customer", Customer.class);
+		//execute the query and get result list
+		
+		List <Customer> customers = theQuery.getResultList();
+		
+		
+		//return the results
+		
+		
+		return null;
+	}
 
 }
