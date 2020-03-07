980a149fee1e1186b29df529a3a833a65ccd38d6
Iredafe Owolabi <iredafeowolabi@gmail.com>
2020-03-06 21:26:15 -0800

    refactor get method using @getmapping

--- src/com/dafe/springdemo/controller/CustomerController.java
+++ src/com/dafe/springdemo/controller/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -22,7 +23,7 @@ public class CustomerController {
 	private CustomerDao customerDao;
 	
 	
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String lisCustomers(Model theModel) {
 	
 		//get the customer from the dao
