63f6b0528611ba50225ac4f89d3a543ee6dac5b8
Iredafe Owolabi <iredafeowolabi@gmail.com>
2020-03-06 21:51:04 -0800

    adding service layer based on service facade design pattern

---
+++ src/com/dafe/springdemo/service/CustomerService.java
@@ -0,0 +1,11 @@
+package com.dafe.springdemo.service;
+
+import java.util.List;
+
+import com.dafe.springdemo.entity.Customer;
+
+public interface CustomerService {
+
+	
+	public List <Customer> getCustomers();
+}
---
+++ src/com/dafe/springdemo/service/CustomerServiceImpl.java
@@ -0,0 +1,15 @@
+package com.dafe.springdemo.service;
+
+import java.util.List;
+
+import com.dafe.springdemo.entity.Customer;
+
+public class CustomerServiceImpl implements CustomerService {
+
+	@Override
+	public List<Customer> getCustomers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
