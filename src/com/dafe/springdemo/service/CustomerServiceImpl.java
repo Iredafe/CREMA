5b8d6817b24a4cbb72105ec4a373e32d0ddb3d3d
Iredafe Owolabi <iredafeowolabi@gmail.com>
2020-03-06 22:03:26 -0800

    adding service layer

--- src/com/dafe/springdemo/service/CustomerServiceImpl.java
+++ src/com/dafe/springdemo/service/CustomerServiceImpl.java
@@ -2,14 +2,29 @@ package com.dafe.springdemo.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dafe.springdemo.dao.CustomerDao;
 import com.dafe.springdemo.entity.Customer;
 
+@Service
 public class CustomerServiceImpl implements CustomerService {
 
+	
+	
+	//inject customer dao
+	@Autowired
+	private CustomerDao customerDao;
+	
+	
+	
 	@Override
+	@Transactional
 	public List<Customer> getCustomers() {
-		// TODO Auto-generated method stub
-		return null;
+	return customerDao.getCustomers();	
+		
 	}
 
 }
